7d8e17cb13e2ee4ec3079b16ab5d8d1896011404
guyettinger <guy@flaire.co>
2025-08-05 20:53:20 -0400

    expo update

--- components/screens/Home/index.tsx
+++ components/screens/Home/index.tsx
@@ -3,7 +3,7 @@ import { Text } from '~/components/primitives/Text'
 
 export function HomeScreen() {
   return (
-    <View className="flex h-full w-full flex-1 flex-col">
+    <View className="p-safe flex h-full w-full flex-1 flex-col">
       <Text className="text-primary">Hello World</Text>
     </View>
   )
