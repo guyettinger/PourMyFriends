87095475022fdb7a88d0f1ce79457069f59b015a
guyettinger <guy@flaire.co>
2025-08-07 12:53:38 -0400

    expo-gl

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { HomeScreen } from 'components/screens/Home'
+import { GLScreen } from '~/components/screens/GL'
 
 export default function Screen() {
-  return <HomeScreen />
+  return <GLScreen />
 }
